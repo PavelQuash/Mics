2f0cb30cd5f96f6c19b5605ed9f386e6b630d6fa
Pavel Quash <quash@e1.ru>
2017-11-06 10:30:59 +0500

    Second var with ds1302

--- sample-room-climate-mon/sample-room-climate-mon.ino
+++ sample-room-climate-mon/sample-room-climate-mon.ino
@@ -1,7 +1,7 @@
 
 
 // DS1302
-#include "RTClib.h"
+#include "DS1302.h" // https://github.com/msparks/arduino-ds1302
 
 // DHT22
 #include "Adafruit_Sensor.h"
@@ -16,26 +16,28 @@
 
 // Инициализация
 DHT dht(DHTPIN, DHT22);
-DS1302 rtc(2,4,3);
+DS1302 rtc(2,3,4); // CLK (CE, Chip Enable) pin, DATA (I/O, Input/Output) pin, RST (SCLK, Serial Clock) pin
 
 void setup() {
   logSetup();
-  log("Start");
-  
   tempMonHumSetup();
-  rtc.begin();
+  rtcSetup();
+  log("Start");
 }
 
 void loop() {
-  rtc.adjust(DateTime(__DATE__, __TIME__));
-  DateTime now = rtc.now();
-  showDate(now);  
-  delay(2000);
+  //tempMonHumCycle();
+  rtcCycle();
 }
 
-void tempMonHumSetup()
-{
-  dht.begin();
+
+
+void tempMonHumSetup() { dht.begin(); }
+void rtcSetup() { 
+  rtc.writeProtect(false);
+  rtc.halt(false);
+  //Time t(2017, 11, 6, 10, 20, 00, Time::kSunday); // Make a new time object to set the date and time.
+  //rtc.time(t); // Set the time and date on the chip.
 }
 
 void tempMonHumCycle()
@@ -50,31 +52,48 @@ void tempMonHumCycle()
   Serial.println("Hum: " + String(h) + " %\t" + "Temp: " + String(t) + " *C ");
 }
 
-void logSetup()
+
+
+void rtcCycle()
 {
-  Serial.begin(9600);
+  printTime();
+  delay(2000);
 }
 
-void log(String message)
-{
-  Serial.println(message);
+void printTime() {
+  // Get the current time and date from the chip.
+  Time t = rtc.time();
+
+  // Name the day of the week.
+  const String day = dayAsString(t.day);
+
+  // Format the time and date and insert into the temporary buffer.
+  char buf[50];
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+
+  // Print the formatted string to serial so we can see the time.
+  Serial.println(buf);
 }
 
-void showDate(const DateTime& dt) {
-    Serial.print(dt.year(), DEC);
-    Serial.print('/');
-    Serial.print(dt.month(), DEC);
-    Serial.print('/');
-    Serial.print(dt.day(), DEC);
-    Serial.print(' ');
-    Serial.print(dt.hour(), DEC);
-    Serial.print(':');
-    Serial.print(dt.minute(), DEC);
-    Serial.print(':');
-    Serial.print(dt.second(), DEC);
-    
-    Serial.print(" = ");
-    Serial.print(dt.unixtime());
-    Serial.println();
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "Sunday";
+    case Time::kMonday: return "Monday";
+    case Time::kTuesday: return "Tuesday";
+    case Time::kWednesday: return "Wednesday";
+    case Time::kThursday: return "Thursday";
+    case Time::kFriday: return "Friday";
+    case Time::kSaturday: return "Saturday";
+  }
+  return "(unknown day)";
 }
+
+
+
+void logSetup() { Serial.begin(9600); }
+void log(String message) { Serial.println(message); }
+
 
