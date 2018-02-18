32a77179b80f136f40245fd260e6d52db30ae9f5
Quash <quash@e1.ru>
2018-02-18 16:44:07 +0500

    Add pressure sensors and datasheets to compare with other sensors. Add empty garage project.

--- _hardware/_drivers/README.md
+++
@@ -1,5 +0,0 @@
-## NodeMcu IDE Setup
-http://arduino-project.net/nodemcu-v3-arduino-ide/
-Arduino IDE -> File -> Settings
-Additional Boards Manager Urls:
-http://arduino.esp8266.com/versions/2.3.0/package_esp8266com_index.json
--- project-workspace-climate-monitor/project-workspace-climate-monitor.ino
+++ project-workspace-climate-monitor/project-workspace-climate-monitor.ino
@@ -1,6 +1,26 @@
+// Created by Quash (qshzone.ru) 2018
+// 350 — 450 ppm    : Нормальный уровень на открытом воздухе.
+// less than 600 ppm : Приемлемые уровни. Уровень. рекомендованный для спален, детских садов и школ.
+// 600 — 1000 ppm   : Жалобы на несвежий воздух, возможно снижение концентрации внимания.
+// 1000 ppm          : Максимальный уровень стандартов ASHRAE (American Society of Heating, Refrigerating and Air-Conditioning Engineers) и OSHA (Occupational Safety & Health Administration).
+// 1000 — 2500 ppm  : Общая вялость, снижение концентрации внимания, возможна головная боль.
+// 2500 — 5000 ppm  : Возможны нежелательные эффекты на здоровье.
+// dht22 
+// mh-z19 +/- 50 ppm
+
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_BMP085.h>
+#include <Adafruit_BME280.h>
+
+// Roadmap
+// 1. Noise read value, and score it
+// 2. Different threads without common delay
+// 3. Updating screen just values and if value is changed
+// 4. Delete ppm>m< symbol
+// 5. Timer to turn off lcd backlight by button
+// 6. Send data to thingspeak service
 
 // Settings
 LiquidCrystal_I2C lcd(0x27,20,4);  // Address and size of display
@@ -13,11 +33,13 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // Address and size of display
 #define PERIOD_DHT22 2000 // Not often than 2 secs
 #define PERIOD_MHZ19 3000 // Not often than 3 secs
 #define PERIOD_BUZZER 12000 // 2 minutes
-#define MAX_NOISE_SCORE 10
+#define MAX_NOISE_SCORE 5
 
 // Initialize
 DHT dht(DHT_PIN, DHT22);
 SoftwareSerial mhz19serial(MHZ19_TX_PIN, MHZ19_RX_PIN); // RX, TX Nano
+Adafruit_BMP085 bmp;
+Adafruit_BME280 bme;
 unsigned long dht22LastRead = 0;
 unsigned long mhz19LastRead = 0;
 unsigned long buzzerLastRead = 0;
@@ -34,29 +56,57 @@ byte noise_symbol[8] = // :(
 };     
 
 void setup() {
+  // Logging
+  logSetup();
+  log("Start");
   // Display
   lcd.init(); // Initialize the lcd
   lcd.backlight(); // Turn on the backlight 
   lcd.createChar(1, noise_symbol);
+  lcd.clear();
+  lcd.setCursor(0,0); lcd.print("Tmp ");
+  lcd.setCursor(0,1); lcd.print("Hum ");
+  lcd.setCursor(0,2); lcd.print("CO2 ");
+  lcd.setCursor(0,3); lcd.print("Tmp ");
+  lcd.setCursor(10,0); lcd.print("Tmp "); 
+  lcd.setCursor(10,1); lcd.print("Hum "); 
+  lcd.setCursor(12,2); lcd.print("A "); 
+  lcd.setCursor(10,3); lcd.print("Prs "); 
   // Sensors
   dht.begin();
   mhz19serial.begin(9600);
   pinMode(NOISE_PIN, INPUT);
   pinMode(SOUND_PIN, OUTPUT);
-  // Logging
-  logSetup();
-  log("Start");
+  
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  if (!bme.begin()) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
 }
 
 void loop() {
   // Reading data from sensors
-  float h = dht.readHumidity();
   float t = dht.readTemperature();
+  float h = dht.readHumidity();
   dht22LastRead = millis();
   int co2 = readCO2();
   mhz19LastRead = millis();
-  int noise = digitalRead(NOISE_PIN);
+  int noise = 1;//digitalRead(NOISE_PIN);
   buzzerLastRead = millis();
+
+  log("\tTemp(C)\tHum(%)\tPrs(mm)\tAlt(m)\tCO2(ppm)");
+  log("dht\t" + String(t,1) + "\t" + String(h,1) + "\t-\t-\t-");
+  log("bme\t" + String(bme.readTemperature()) + "\t" + String(bme.readHumidity()) + "\t" + String(bme.readPressure() * 0.007501) + "\t" + String(bme.readAltitude(1013.25)) + "\t-");
+  log("bmp\t" + String(bmp.readTemperature()) + "\t-\t" + String(bmp.readPressure() * 0.007501) + "\t" + String(bmp.readAltitude()) + "\t-");
+  log("mhz\t-\t-\t-\t-\t" + String(co2));
+  log("");
   
   if (noise == 0) {
     if (noise_score == MAX_NOISE_SCORE) { playMelody(); }
@@ -65,30 +115,31 @@ void loop() {
   else if (noise_score > 0) { 
     noise_score--;
   }
-  log("Nse " + String(noise)); 
+  //log("Noise sensor " + String(noise)); 
 
   // Show data
-  lcd.clear();
-  lcd.setCursor(0,0); 
-  lcd.print("Tmp ");
-  if (!isnan(h)) { lcd.print(String(t) + "C"); }
-  lcd.setCursor(0,1);
-  lcd.print("Hum ");
-  if(!isnan(t)) { lcd.print(String(h) + "%"); }
-  lcd.setCursor(0,2);
-  lcd.print("CO2 ");
-  if (co2 >= 0) { lcd.print(String(co2) + "ppm"); }
-  lcd.setCursor(0,3);
-  lcd.print("Nse "); 
-  for(int i = 0; i < noise_score; i++) { lcd.print("\1"); }
+  lcd.setCursor(4,0); 
+  if (!isnan(h)) { lcd.print(String(t,1) + "C"); }
+  lcd.setCursor(4,1);
+  if(!isnan(t)) { lcd.print(String(h,1) + "%"); }
+  lcd.setCursor(4,2);
+  if (co2 >= 0) { lcd.print(String(co2) + "ppm "); }
+  //lcd.setCursor(15,0);
+  //for(int i = 0; i < noise_score; i++) { lcd.print("\1"); }
+
+  lcd.setCursor(14,0); lcd.print(String(bme.readTemperature()) + "C");
+  lcd.setCursor(14,1); lcd.print(String(bme.readHumidity()) + "%");
+  lcd.setCursor(14,2); lcd.print(String(bme.readAltitude(1013.25)));;// + "m ");
+  lcd.setCursor(14,3); lcd.print(String(bme.readPressure() * 0.007501));//+ "mm ");
 
   delay(10000);
 }
 
 // MH-Z19 reading co2 value
-byte mhz19_command[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}; // MH-Z19 comand to get data
-unsigned char mhz19_response[9];
 int readCO2() {
+  byte mhz19_command[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79}; // MH-Z19 comand to get data
+  unsigned char mhz19_response[9];
+  
   mhz19serial.write(mhz19_command, 9);
   memset(mhz19_response, 0, 9);
   mhz19serial.readBytes(mhz19_response, 9);
@@ -103,6 +154,9 @@ int readCO2() {
   // Result value calculating
   unsigned int responseHigh = (unsigned int)mhz19_response[2];
   unsigned int responseLow = (unsigned int)mhz19_response[3];
+  
+  lcd.setCursor(4,3); lcd.print(String(mhz19_response[4] - 40) + "C");
+  
   return 256 * responseHigh + responseLow;
 }
 
